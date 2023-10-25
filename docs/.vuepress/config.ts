9e987fb4bb6b064737681ff1e6e955ec489e8323
HuberyYang <18830279823@163.com>
2023-10-25 15:56:04 +0800

    配置gitalk

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -324,11 +324,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: "gitalk",
         options: {
-          clientID: "a6e1355287947096b88b",
-          clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
-          repo: "blog-gitalk-comment", // GitHub 仓库
-          owner: "xugaoyi", // GitHub仓库所有者
-          admin: ["xugaoyi"], // 对仓库有写权限的人
+          clientID: "333f590fbed08041a81d",
+          clientSecret: "adff319df27f9fbdebb5698d843ea94a301650c8",
+          repo: "my-blog-new", // GitHub 仓库
+          owner: "Hub-yang", // GitHub仓库所有者
+          admin: ["Hub-yang"], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: "last", // 'first'正序 | 'last'倒序
           id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
